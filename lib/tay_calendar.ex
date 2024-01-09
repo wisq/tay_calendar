e5e12d6d809611ce517fd3727d54833c84a61996
Adrian Irving-Beer <wisq@wisq.net>
2024-01-08 23:10:48 -0500

    `mix new tay_calendar`

---
+++ lib/tay_calendar.ex
@@ -0,0 +1,18 @@
+defmodule TayCalendar do
+  @moduledoc """
+  Documentation for `TayCalendar`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> TayCalendar.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/tay_calendar_test.exs
@@ -0,0 +1,8 @@
+defmodule TayCalendarTest do
+  use ExUnit.Case
+  doctest TayCalendar
+
+  test "greets the world" do
+    assert TayCalendar.hello() == :world
+  end
+end
