70b142e2538a3f7287f9f98d3f45f2f4cd244948
Adrian Irving-Beer <wisq@wisq.net>
2024-01-12 03:17:11 -0500

    Allow storing tzdata somewhere writable.

--- config/runtime.exs
+++ config/runtime.exs
@@ -4,3 +4,8 @@ alias TayCalendar.Secrets
 if config_env() in [:dev, :prod] do
   config :google_maps, api_key: Secrets.fetch!("GOOGLE_MAPS_API_KEY")
 end
+
+case System.fetch_env("TZDATA_DATA_DIR") do
+  {:ok, path} -> config :tzdata, :data_dir, path
+  :error -> :noop
+end
