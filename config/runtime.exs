fd90934c74be134a72090546df2d4f3d3dcc2b96
Adrian Irving-Beer <wisq@wisq.net>
2024-02-04 03:02:39 -0500

    Remove remaining google maps config.

--- config/runtime.exs
+++ config/runtime.exs
@@ -1,10 +1,6 @@
 import Config
 alias TayCalendar.Secrets
 
-if config_env() in [:dev, :prod] do
-  config :google_maps, api_key: Secrets.fetch!("GOOGLE_MAPS_API_KEY")
-end
-
 case System.fetch_env("TZDATA_DATA_DIR") do
   {:ok, path} -> config :tzdata, :data_dir, path
   :error -> :noop
